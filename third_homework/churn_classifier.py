7b589fe77707e1447606a7f814043315e903a408
yana-samoylova <79416422+yana-samoylova@users.noreply.github.com>
2021-09-07 03:24:22 +0300

    final

---
+++ third_homework/churn_classifier.py
@@ -0,0 +1,19 @@
+import joblib
+import pandas as pd
+from xgboost import XGBClassifier
+
+
+class ChurnClassifier(object):
+    def __init__(self):
+    # подгрузка модели
+        self.model = joblib.load('model (1).pkl')
+
+    def predict_customer_churn(self, predictable_df):
+    # предсказание оттока
+        try:
+            predictable_df['Churn'] = self.model.predict(predictable_df)
+            predicted_df = predictable_df
+            return predicted_df
+        except:
+            print('Failed to predict')
+            return None
